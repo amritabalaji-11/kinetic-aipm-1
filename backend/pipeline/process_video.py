a6356b54da355c888f01f010f908c1e1eb13dc6d
Valetin <vncaselli@gmail.com>
2026-05-12 15:02:39 -0300

    process_video implementation 2

--- backend/pipeline/process_video.py
+++ backend/pipeline/process_video.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from db.database import db
 
 
 async def process_video(
@@ -6,19 +8,57 @@ async def process_video(
     analysis_id: str
 ) -> dict:
 
-    # Simulación de procesamiento
-    await asyncio.sleep(2)
+    try:
 
-    return {
-        "status": "success",
-        "overlay_video_url": f"gs://kinetic_bucket/overlay/{analysis_id}.mp4",
-        "biomechanics_json": {
-            "rep_count": 8,
-            "overall_score": 72,
-            "exercise": "squat",
-            "feedback": [
-                "Good squat depth",
-                "Keep chest more upright"
-            ]
+        #Pipeline Simulation
+        await asyncio.sleep(5)
+
+        result = {
+            "status": "success",
+            "overlay_video_url": f"gs://kinetic_bucket/overlay/{analysis_id}.mp4",
+            "biomechanics_json": {
+                "rep_count": 8,
+                "overall_score": 72,
+                "exercise": "squat"
+            }
         }
-    }
+
+        # Save in DB
+        await db.execute(
+            query="""
+            UPDATE form_sessions
+            SET
+                status = :status,
+                overlay_video_url = :overlay,
+                biomechanics_json = :biomechanics
+            WHERE session_id = :session_id
+            """,
+            values={
+                "status": "completed",
+                "overlay": result["overlay_video_url"],
+                "biomechanics": json.dumps(result["biomechanics_json"]),
+                "session_id": analysis_id
+            }
+        )
+
+        return result
+
+    except Exception:
+
+        await db.execute(
+            query="""
+            UPDATE form_sessions
+            SET status = :status
+            WHERE session_id = :session_id
+            """,
+            values={
+                "status": "failed",
+                "session_id": analysis_id
+            }
+        )
+
+        return {
+            "status": "failed",
+            "error_code": "PIPELINE_ERROR",
+            "affected_landmarks": []
+        }
